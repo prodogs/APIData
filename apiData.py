c966144f9d8a40114dfadb20e86c402cd39905ba
Frank Filippis <frank@filippis.com>
2024-02-16 23:03:16 -0500

    aa

--- apiData.py
+++ apiData.py
@@ -7,16 +7,48 @@ from datetime import datetime
 def generateAllocation():
     numOfFunds = random.randint(1,5)
     allocation = []
+    allocationString = "{ allocation: ["
     for i in range(0,numOfFunds):
         theFund = funds[i]
         allocationPercent = 100/numOfFunds
+        allocationString += "{{'fundCode': '{}', 'percent': {}}},".format(theFund, allocationPercent)
+
         allocation.append({"fund": theFund, "percent": allocationPercent})
 
-    allocationJson = json.dumps(allocation)
-    return allocationJson
+    allocationString += "]}"
+    return allocationString
+
+
+
+
+
+
 
 # Specify the path to your JSON file
 file_path = "./api_account.json"
+
+
+ 
+funds = ["AAA","BBB","CCC","DDD","EEE", "FFFF"]
+outData = []
+for x in range(0,1000):
+    allocationString = generateAllocation()
+    random_int = random.randint(1, 100000)
+    current_time = datetime.now()
+    
+    timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    apiRow = {'id': random_int, 'timeStamp ': timestamp,'type': "ALLOCATION", 'package': allocationString }
+    outData.append(apiRow)  
+    print(apiRow)
+        
+  # Convert the data to a DataFrame
+df = pd.DataFrame(outData)
+
+    # Save the DataFrame to a CSV file
+df.to_csv('api_allocation.csv', index=False)
+    
+exit()
+
 
 # create account API Activity Data
 with open(file_path, "r") as file:
@@ -34,22 +66,4 @@ with open(file_path, "r") as file:
         apiRow = {'id': random_int, 'timeStamp ': stamp,'type': "ACCOUNT_OPEN", 'package': i }
         outData.append(apiRow)
 
-  
-    funds = ["AAA","BBB","CCC","DDD","EEE", "FFFF"]
-
-    for x in range(0,1000):
-        allocation = []
-        allocation.append(generateAllocation())
-        allocationJson = { "account" : "XXXX", "allocation" : allocation }
-        random_int = random.randint(1, 100000)
-        apiRow = {'id': random_int, 'timeStamp ': stamp,'type': "ALLOCATION", 'package': allocationJson }
-        outData.append(apiRow)  
-        print(apiRow)
-        
-        
-  # Convert the data to a DataFrame
-    df = pd.DataFrame(outData)
-
-    # Save the DataFrame to a CSV file
-    df.to_csv('api_data.csv', index=False)
-    
+ 
